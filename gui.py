331c4b6bda7ef36205cc130ca084f6153d2ac760
Zaved <zavedahmadca707@gmail.com>
2025-03-15 19:08:54 +0530

    added spinner

--- gui.py
+++ gui.py
@@ -9,9 +9,11 @@ import main
 initial_dir = os.getcwd()
 
 
-button=st.button("run")
+button=st.button("Run?")
 if button:
     json_file="D:\zaved\pythonprojects\gemini\last_output.json"
     source_dir=os.path.join('D:\zaved\pythonprojects\gemini', 'ios_icons')
-    if(main.dothething(source_dir, source_dir,json_file)):   
-     st.write("done")
+    with st.spinner("Gemini is working..."):
+        if(main.dothething(source_dir, source_dir,json_file)):   
+            st.write("done")
+        
