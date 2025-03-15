103580ebaba3cb067e1c0434386724d9c229107f
Zaved <zavedahmadca707@gmail.com>
2025-03-15 20:33:00 +0530

    added pydantic schema in #gui.py -Added text box for file source input

--- gui.py
+++ gui.py
@@ -9,11 +9,31 @@ import main
 initial_dir = os.getcwd()
 
 
+# gui to get the source and destination directory
+source_dir=os.path.join('D:\zaved\pythonprojects\gemini', 'ios_icons')
+source_dir=st.text_input("Enter the path of the source directory",value=source_dir)
+if os.path.exists(source_dir):
+    path1_validated=True
+    st.write("Passing")
+else:
+    path1_validated=False
+    st.write("Invalid path")
+destination_dir=st.text_input("Enter the path of the destination directory",value=source_dir)
+if os.path.exists(destination_dir):
+    path2_validated=True
+    st.write("Passing")
+else:
+    path2_validated=False
+    st.write("Invalid path")
 button=st.button("Run?")
+# button pressed
 if button:
-    json_file="D:\zaved\pythonprojects\gemini\last_output.json"
-    source_dir=os.path.join('D:\zaved\pythonprojects\gemini', 'ios_icons')
-    with st.spinner("Gemini is working..."):
-        if(main.dothething(source_dir, source_dir,json_file)):   
-            st.write("done")
+    if path1_validated and path2_validated:
+        json_file="D:\zaved\pythonprojects\gemini\last_output.json"
+        
+        with st.spinner("Gemini is working..."):
+            if(main.dothething(source_dir, source_dir,json_file)):   
+                st.write("done")
+    else:
+        st.write("Bad choice")
         
--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from dotenv import load_dotenv
 import os
 import files 
 import json
+from pydantic import BaseModel
+#this defines the scheme{https://ai.google.dev/gemini-api/docs/structured-output?lang=python#define_a_schema_with_a_type} for the output from the ai
+class rule(BaseModel):
+  file_name: str
+  destination_folder: str
+
 
 
 def get_response(files_list, GEMINI_API_KEY):
@@ -10,19 +16,12 @@ def get_response(files_list, GEMINI_API_KEY):
     client = genai.Client(api_key=GEMINI_API_KEY)
     response = client.models.generate_content(
         model="gemini-2.0-flash",
-        contents='''Organize these files into folders based on their type or purpose. For each file, provide a JSON with two fields: json_path and destination_folder. Use categories like Images, Documents, Audio, etc. Output only the JSON, like this: '
-        '[
-    {"file_name": "photo.jpg", 
-    "destination_folder": "Images"},
-    {"file_name": "report.pdf", 
-    "destination_folder": "Documents"},
-    {"file_name": "song.mp3",
-        "destination_folder": "Audio"}
-        ]''
-        'here are files'''+files_list,
+        contents='''Organize these files into folders based on their type or purpose. For each file, provide a JSON with two fields: json_path and destination_folder. Use categories like Images, Documents, Audio, etc. Output only the JSON, '
+        '''+files_list,
         config={
             'temperature': 0.1,
             'response_mime_type': 'application/json',
+            'response_schema': list[rule]
         })
     return response.text
 
@@ -43,8 +42,9 @@ def dothething(source_dir, destination_dir,json_path):
         print(f"The file '{json_path}' already exists. Deleting it...")
     os.remove(json_path)  # Delete the file
 
+    
+    files.organize_files(json_file,source_dir,destination_dir)
     with open(json_path, "w") as file:
         json.dump(json_file, file, indent=4)
-    files.organize_files(json_file,source_dir,destination_dir)
     return True
 
